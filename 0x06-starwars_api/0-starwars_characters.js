bb97be7aeafcaa8a7715a0b42cbca5728a2394d2
NjugunaW <njugunawangui10@gmail.com>
2024-02-15 21:57:22 +0300

    Updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,44 +5,49 @@ const request = require('request');
 const movieId = process.argv[2];
 const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 let people = [];
+const names = [];
 
-const requestCharacters = () => {
-  return new Promise((resolve, reject) => {
-    request(filmEndPoint, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        reject('Error fetching movie data. StatusCode: ' + res.statusCode);
-      } else {
-        const jsonBody = JSON.parse(body);
-        people = jsonBody.characters;
-        resolve();
-      }
-    });
-  });
-};
-
-const requestNames = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        reject('Error fetching character data. StatusCode: ' + res.statusCode);
-      } else {
-        const jsonBody = JSON.parse(body);
-        console.log(jsonBody.name);
-        resolve();
-      }
-    });
-  });
-};
-
-const getCharacterNames = async () => {
-  try {
-    await requestCharacters();
-    for (const characterUrl of people) {
-      await requestNames(characterUrl);
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
     }
-  } catch (error) {
-    console.error(error);
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters for some reason');
   }
 };
 
-getCharacterNames();
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
